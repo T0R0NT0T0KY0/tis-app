ee57436cc7028dd1bead1c420dfc2c16fdcf78ec
Булатов Дмитрий Алексеевич <bulatovda@sovcombank.ru>
2021-10-30 23:47:54 +0300

    #9 user page was added

--- user_page/index.js
+++ user_page/index.js
@@ -4,20 +4,20 @@ const getQuery = () => {
 
 export const setNickname = () => {
     const nickname = document.querySelector(".nickname");
-    fetch('https://192.1668.1.3/api/user/nickname?' + getQuery())
+    fetch('https://192.168.1.3/api/user/nickname?' + getQuery())
         .then(r => nickname.textContent = r.json().data.nickname)
         .catch((e) => nickname.textContent = "")
 }
 export const setUsername = () => {
     const username = document.querySelector(".username");
-    fetch('https://192.1668.1.3/api/user/username?'  + getQuery())
+    fetch('https://192.168.1.3/api/user/username?'  + getQuery())
         .then(r => username.textContent = r.json().data.username)
         .catch((e) => username.textContent = "")
 }
 
 export const setTeam = () => {
     const nickname = document.querySelector(".team_name");
-    fetch('https://192.1668.1.3/api/user/team?'  + getQuery())
+    fetch('https://192.168.1.3/api/user/team?'  + getQuery())
         .then(r => nickname.textContent = r.json().data.team)
         .catch((e) => nickname.textContent = "")
 
@@ -25,7 +25,7 @@ export const setTeam = () => {
 
 export const setLivingPlace = () => {
     const nickname = document.querySelector(".living_place");
-    fetch('https://192.1668.1.3/api/user/living_place?'  + getQuery())
+    fetch('https://192.168.1.3/api/user/living_place?'  + getQuery())
         .then(r => nickname.textContent = r.json().data.place)
         .catch((e) => nickname.textContent = "")
 
@@ -33,7 +33,7 @@ export const setLivingPlace = () => {
 
 export const setBirthday = () => {
     const nickname = document.querySelector(".birthday");
-    fetch('https://192.1668.1.3/api/user/birthday?'  + getQuery())
+    fetch('https://192.168.1.3/api/user/birthday?'  + getQuery())
         .then(r => nickname.textContent = r.json().data.birthday)
         .catch((e) => nickname.textContent = "")
 
@@ -41,7 +41,7 @@ export const setBirthday = () => {
 
 export const setEmail = () => {
     const nickname = document.querySelector(".email");
-    fetch('https://192.1668.1.3/api/user/email?'  + getQuery())
+    fetch('https://192.168.1.3/api/user/email?'  + getQuery())
         .then(r => nickname.textContent = r.json().data.email)
         .catch((e) => nickname.textContent = "")
 
@@ -49,7 +49,7 @@ export const setEmail = () => {
 
 export const setAbout = () => {//todo
     const nickname = document.querySelector(".about_info");
-    fetch('https://192.1668.1.3/api/user/email?'  + getQuery())
+    fetch('https://192.168.1.3/api/user/email?'  + getQuery())
         .then(r => nickname.textContent = r.json().data.email)
         .catch((e) => nickname.textContent = "")
 
