4a152bf8c597b21af6c16f4fe1647a1810fb7317
Булатов Дмитрий Алексеевич <bulatovda@sovcombank.ru>
2021-10-30 23:47:25 +0300

    #9 user page was added

--- registration/js/main.js
+++ registration/js/main.js
@@ -1,14 +1,14 @@
 (function ($) {
         "use strict";
-        
-        
+
+
         /*==================================================================
          [ Validate ]*/
         var input = $('.validate-input .input100');
-        
+
         $('.validate-form').on('submit', function () {
             let check = true;
-            
+
             for (let i = 0; i < input.length; i++) {
                 if (!validate(input[i])) {
                     showValidate(input[i]);
@@ -18,14 +18,14 @@
             if (check) ajax_post_request(input[0].value, input[1].value, input[2].value, input[3].value);
             return check;
         });
-        
-        
+
+
         $('.validate-form .input100').each(function () {
             $(this).focus(function () {
                 hideValidate(this);
             });
         });
-        
+
         function validate (input) {
             if ($(input).attr('type') === 'email' || $(input).attr('name') === 'email') {
                 if ($(input).val().trim().match(/^([a-zA-Z0-9_\-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(]?)$/) == null) {
@@ -38,19 +38,19 @@
             }
             return true;
         }
-        
+
         function showValidate (input) {
             var thisAlert = $(input).parent();
-            
+
             $(thisAlert).addClass('alert-validate');
         }
-        
+
         function hideValidate (input) {
             var thisAlert = $(input).parent();
-            
+
             $(thisAlert).removeClass('alert-validate');
         }
-        
+
         const ajax_post_request = (username, nickname, email, password) => {
             alert(JSON.stringify({ name: username, nick: nickname, email, password }))
             $.ajax({
@@ -58,7 +58,7 @@
                 method: 'POST',
                 data: JSON.stringify({ username, nickname, email, password }),
                 success: () => {
-                    //todo
+                    window.location.href = "../"
                 }
             })
         }
