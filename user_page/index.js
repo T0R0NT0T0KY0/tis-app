c8067e5498c9416ca6ffcfa09cb2fe209971e280
Булатов Дмитрий Алексеевич <bulatovda@sovcombank.ru>
2021-11-02 18:18:02 +0300

    #9 user page was updated

--- user_page/index.js
+++ user_page/index.js
@@ -1,71 +1,90 @@
 const getQuery = () => {
-    return window.location.search.split("?")[1] ?? "";
+    return window.location.search ?? "";
 }
+const base_url = 'http://192.168.1.3:8080/api/user';
 
 const setNickname = async () => {
     const nickname = document.querySelector(".nickname");
-    await fetch('https://192.168.1.3/api/user/nickname?' + getQuery())
-        .then(r => {
-            console.log(3232, {r})
-            return nickname.textContent = r.json().data.nickname
-        })
-        .catch((e) => nickname.textContent = "")
+     await fetch(base_url +'/nickname' + getQuery())
+        .then(resp => console.log({resp}))
+        .catch(e => console.log({e}));
+        // .then(r => {
+        //     console.log(1, r.body)
+        //     return r.body
+        // })
+        // .then(r => nickname.textContent = r.data.nickname)
+        // .catch((e) => nickname.textContent = e)
 }
-const setUsername = () => {
+const setUsername = async () => {
     const username = document.querySelector(".username");
-    fetch({url:'https://192.168.1.3/api/user/username?'  +getQuery()})
-        .then(r => username.textContent = r.json().data.username)
-        .catch((e) => username.textContent = "")
+    await fetch({url: base_url + '/username' + getQuery()})
+        .then(r => {
+            console.log(2, r.body)
+            return r.json()
+        })
+        .then(r => {
+            return username.textContent = r.data.username
+        })
+        .catch((e) => username.textContent = e)
 }
 
-const setTeam = () => {
+const setTeam = async () => {
     const nickname = document.querySelector(".team_name");
-    fetch('https://192.168.1.3/api/user/team?'  + getQuery())
+    await fetch(base_url + '/team' + getQuery())
         .then(r => nickname.textContent = r.json().data.team)
         .catch((e) => nickname.textContent = "")
-
 }
 
-const setLivingPlace = () => {
+const setLivingPlace = async () => {
     const living_place = document.querySelector(".living_place");
-    fetch('https://192.168.1.3/api/user/living_place?'  + getQuery())
+    await fetch(base_url + '/living_place' + getQuery())
         .then(r => living_place.textContent = r.json().data.place)
         .catch((e) => living_place.textContent = "")
 
 }
 
-const setBirthday = () => {
+const setBirthday = async () => {
     const birthday = document.querySelector(".birthday");
-    fetch('https://192.168.1.3/api/user/birthday?'  + getQuery())
+    await fetch(base_url + '/birthday' + getQuery())
         .then(r => birthday.textContent = r.json().data.birthday)
         .catch((e) => birthday.textContent = "")
-
 }
 
-const setEmail = () => {
+const setEmail = async () => {
     const email = document.querySelector(".email");
-    fetch('https://192.168.1.3/api/user/email?'  + getQuery())
+    await fetch(base_url + '/email' + getQuery())
         .then(r => email.textContent = r.json().data.email)
         .catch((e) => email.textContent = "")
 
 }
 
-const setAbout = () => {//todo
+const setAbout = async () => {
     const about = document.querySelector(".about_info");
-    fetch('https://192.168.1.3/api/user/about?'  + getQuery())
+    await fetch(base_url + '/about' + getQuery())
         .then(r => about.textContent = r.json().data.about)
         .catch((e) => about.textContent = "")
 
 }
-const setImage = () => {//todo
+const setImage = async () => {
     const img = document.querySelector(".user_image");
-    fetch('https://192.168.1.3/api/user/photo?'  + getQuery())
+    await fetch(base_url + '/photo' + getQuery())
         .then(r => r.json())
         .then(json => img.setAttribute("src", json.data.link))
         .catch((e) => img.textContent = "")
 
 }
 
-module.exports = {setNickname, setAbout, setBirthday, setEmail, setTeam, setLivingPlace, setImage};
+const setData = async () => {
+    await setNickname();
+    await setUsername();
+    await setTeam();
+    await setLivingPlace()
+    await setBirthday()
+    await setEmail();
+    await setAbout();
+    await setImage();
+};
+
+module.exports = setData;
 
 
